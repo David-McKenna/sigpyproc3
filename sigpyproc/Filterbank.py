Numpy doesn't accept slice objects? :( Also fix another off-by-one

sigpyproc/Filterbank.py
@@ -809,7 +809,7 @@ class FilterbankBlock(np.ndarray):
             delays = self.header.getDMdelays(dm)
             new_ar = FilterbankBlock(np.zeros((self.header.nchans, self.shape[1] - delays[-1]), dtype = self.dtype), self.header)
             end_sample = delays + new_ar.shape[1]
-            slices = [slice(sample, sample + delays[i]) for i, sample in enumerate(delays)]
+            slices = [np.arange(sample, sample + delays[i]) for i, sample in enumerate(delays)]
 
             new_ar[...] = self[np.arange(self.shape[0]), slices] 
             

sigpyproc/Readers.py
@@ -95,7 +95,7 @@ class FilReader(Filterbank):
         lowest_chan, highest_chan, sample_offset = (0, 0, start) 
         with tqdm(total = nsamps * self.header.nchans) as progress:
             while curr_sample[-1] <= nsamps:
-                relevant_channels = np.argwhere(np.logical_and(max_sample > sample_offset, min_sample <= sample_offset)).flatten()
+                relevant_channels = np.argwhere(np.logical_and(max_sample >= sample_offset, min_sample <= sample_offset)).flatten()
                 lowest_chan = np.min(relevant_channels)
                 highest_chan = np.max(relevant_channels)
                 sampled_chans = np.arange(lowest_chan, highest_chan + 1, dtype = int)